99959bfecb4552620e044c2ca0ab2e8719648859
osamudiamen <osas.azamegbe@gmail.com>
2020-07-21 13:20:30 +0100

    testing javascript image request implementation

---
+++ objectsize_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\osamudiamen\documents\vscode projects\freelance\shopify screen dimensions\image object dimensions api\objectsize_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
