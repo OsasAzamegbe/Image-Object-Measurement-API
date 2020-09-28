94677617e134d78d30fc1de9c7fa9dbc7b24e4e2
osamudiamen <osas.azamegbe@gmail.com>
2020-09-28 22:35:48 +0100

    set environmental variables

--- ObjectSize/ObjectSize/settings.py
+++ ObjectSize/ObjectSize/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i_(2j1$$%h6y2_@+a@s)rp*!=p9#5$f#wgr18y@e5e2vzeoao9'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
