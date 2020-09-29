4fab5df261736c98850bcb682e74248b95f3cca2
osamudiamen <osas.azamegbe@gmail.com>
2020-09-29 17:03:52 +0100

    prod settings

--- ObjectSize/ObjectSize/settings.py
+++ ObjectSize/ObjectSize/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'imgobjectmeasurement.herokuapp.com', 'imgobjectmeasurement-staging.herokuapp.com', 'localhost']
 
